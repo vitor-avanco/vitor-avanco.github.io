03411ffd21d7a320854b0f41f3dc0b93032d8df9
Vitor Avanço <vitor.avanco@hotmail.com>
2017-04-06 23:23:49 -0300

    url jason

--- assets/js/homeController.js
+++ assets/js/homeController.js
@@ -1,6 +1,6 @@
 angular.module('portfolio').controller('HomeController', function($http, $scope){
 	
-	$http.get('http://localhost:8080/vitor-avanco.github.io/trabalhos.json').then(successCallback, errorCallback);
+	$http.get('trabalhos.json').then(successCallback, errorCallback);
 	function successCallback(trabalho) { 
 	    console.log("sucesso");
 	    $scope.trabalhos = trabalho.data;
